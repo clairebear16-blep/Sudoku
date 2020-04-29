d6ba4686472b02c9b467496312341c0d700af77b
clairew6-blep <clairew6@illinois.edu>
2020-04-29 14:30:43 -0500

    go

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.launch);
+
+        TextView text = findViewById(R.id.Colordoku);
+        String str = "COLORDOKU";
+        SpannableString ss = new SpannableString(str);
+        ForegroundColorSpan red = new ForegroundColorSpan(Color.RED);
+        ForegroundColorSpan yellow = new ForegroundColorSpan(Color.YELLOW);
+        ForegroundColorSpan cyan = new ForegroundColorSpan(Color.CYAN);
+        ForegroundColorSpan blue = new ForegroundColorSpan(Color.BLUE);
+        ForegroundColorSpan purple = new ForegroundColorSpan(Color.MAGENTA);
+        ForegroundColorSpan grey = new ForegroundColorSpan(Color.LTGRAY);
+        ss.setSpan(red, 0, 0, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(yellow, 1, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(cyan, 2, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(blue, 3, 3, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(purple, 4, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(grey, 5, 5, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(red, 6, 6, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(yellow, 7, 7, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(blue, 8, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        text.setText(ss);
     }
 }
