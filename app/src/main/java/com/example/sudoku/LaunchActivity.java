eb0b35bf9828b0c8d1cc8b17891bde2b511327dc
CharlieZ687 <60624113+CharlieZ687@users.noreply.github.com>
2020-04-29 19:37:13 -0500

    Game page buttons installed

--- app/src/main/java/com/example/sudoku/LaunchActivity.java
+++ app/src/main/java/com/example/sudoku/LaunchActivity.java
@@ -4,14 +4,102 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LaunchActivity extends AppCompatActivity {
-
+    View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**TextView textView = findViewById(R.id.Colordoku);
+        String str = "COLORDUKU";
+        SpannableString ss = new SpannableString(str); */
+
+        view = this.getWindow().getDecorView();
+        Button redButton = (Button) findViewById(R.id.redButton);
+        Button orangeButton = (Button) findViewById(R.id.orangeButton);
+        Button yellowButton = (Button) findViewById(R.id.yellowButton);
+        Button limeButton = (Button) findViewById(R.id.limeButton);
+        Button greenButton = (Button) findViewById(R.id.greenButton);
+        Button lightblueButton = (Button) findViewById(R.id.lightblueButton);
+        Button blueButton = (Button) findViewById(R.id.blueButton);
+        Button pinkButton = (Button) findViewById(R.id.purpleButton);
+        Button purpleButton = (Button) findViewById(R.id.pinkButton);
+
+        redButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.setBackgroundResource(R.color.red);
+            }
+        });
+
+        orangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this part of code is subject to change
+                view.setBackgroundResource(R.color.orange);
+            }
+        });
+
+        yellowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this part of code is subject to change
+                view.setBackgroundResource(R.color.yellow);
+            }
+        });
+
+        limeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this part of code is subject to change
+                view.setBackgroundResource(R.color.lime);
+            }
+        });
+
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this part of code is subject to change
+                view.setBackgroundResource(R.color.green);
+            }
+        });
+
+        lightblueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this part of code is subject to change
+                view.setBackgroundResource(R.color.lightblue);
+            }
+        });
+
+        blueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this part of code is subject to change
+                view.setBackgroundResource(R.color.blue);
+            }
+        });
+
+        pinkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this part of code is subject to change
+                view.setBackgroundResource(R.color.pink);
+            }
+        });
+
+        purpleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this part of code is subject to change
+                view.setBackgroundResource(R.color.purple);
+            }
+        });
     }
 }
